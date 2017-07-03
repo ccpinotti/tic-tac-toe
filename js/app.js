ec8466797f38a35e3314d7ca24df5e975f22025d
ccpinotti <ccpinotti@gmail.com>
2017-07-03 11:19:21 -0700

    Basic framwork of game put together

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+  let player = 1;
 
+  $(".box").on('click',function() {
+    if($(this).text() === "" && player === 1){
+      $(this).text("X");
+      player = 2;
+    }else if (player === 2 && $(this).text()===""){
+      $(this).text("0");
+      player = 1;
+    }else($('.message').html('Try another square'));
+
+
+  });
+  //reset button
+  $('button').on('click',function() {
+    $(".box").text("")
+
+  });
 });
