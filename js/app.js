8e44bff703d30f9dc90eae68e38ba5981b55bac7
ccpinotti <ccpinotti@gmail.com>
2017-07-05 16:25:35 -0700

    changed font size for X's and O's

--- js/app.js
+++ js/app.js
@@ -4,13 +4,12 @@ $(document).ready(function() {
 
   $(".box").on('click',function() {
     if($(this).text() === "" && player === 1){
-      $(this).text("X");
+      $(this).text('X');
       player = 2;
     }else if (player === 2 && $(this).text()===""){
       $(this).text("0");
       player = 1;
     }else($('.message').html('Try another square'));
-
 
   });
   //reset button
@@ -25,10 +24,10 @@ setTimeout(fade_out, 5000);
 function fade_out() {
   $(".message").fadeOut().empty();
 }
-// var "row" = ["1","2","3","4","5","6","7","8","9"];
+// let "row2" = ["1","2","3","4","5","6","7","8","9"];
 //
 // function boardDisplay() {
-//     console.log("| "+boardsq[0]+" | "+boardsq[1]+" | "+boardsq[2]+" |");
-//     console.log("| "+boardsq[3]+" | "+boardsq[4]+" | "+boardsq[5]+" |");
-//     console.log("| "+boardsq[6]+" | "+boardsq[7]+" | "+boardsq[8]+" |");
+//     console.log("| "+row[0]+" | "+row[1]+" | "+row[2]+" |");
+//     console.log("| "+row[3]+" | "+row[4]+" | "+row[5]+" |");
+//     console.log("| "+row[6]+" | "+row[7]+" | "+row[8]+" |");
 // }
